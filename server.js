91fe379882fad5d5aae425267d1888e5a66f0241
DukeHdez <briayan27@outlook.com>
2022-05-17 20:04:47 -0400

    Fix port for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { animals } = require('./data/animals.json')
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -58,6 +59,6 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
 })
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
 });
